caf1332abf69d034a173f4de5e0fab2f24f6f72b
Jay Patel <64770489+Jay-1910@users.noreply.github.com>
2023-08-23 16:07:28 +0930

    Add files via upload

--- activity_log.py
+++ activity_log.py
@@ -1,12 +1,23 @@
 import datetime
 import os
+import geocoder
 
 def log_activity(username, activity):
     activity_log_file = get_activity_log_filename()
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    log_entry = f"{timestamp}: {username} - {activity}\n"
+    
+    # Get the user's live location (city name)
+    user_location = get_user_location()
+    
+    log_entry = f"{timestamp}: {username} - {activity} - Location: {user_location}\n"
+    
     with open(activity_log_file, "a") as file:
         file.write(log_entry)
+
+def get_user_location():
+    g = geocoder.ip("me")
+    city = g.city  # Get the city name
+    return city
 
 def get_activity_log():
     activity_log_file = get_activity_log_filename()
@@ -23,4 +34,3 @@ def get_activity_log_filename():
         with open(activity_log_file, "w") as file:
             file.write("User Activity Log\n\n")
     return activity_log_file
-
--- vineyard_app.py
+++ vineyard_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from predict_page import show_pred
 from dashboard import show_dashboard
 from PIL import Image
-import activity_log  # Import the activity_log module
+import activity_log 
 
 # Load and resize the logo
 image_path = "DTA Logo.png"
@@ -11,8 +11,8 @@ new_size = (160, 100)
 logo = logo.resize(new_size)
 
 # Define valid credentials
-valid_username = ["jay","genene"]
-valid_password = "DTA"
+valid_username = ["jay", "genene"]
+valid_password = ["DTA"]
 
 # Check if user is logged in
 if 'logged_in' not in st.session_state:
@@ -23,12 +23,13 @@ selected_page = None
 
 def show_sidebar():
     global selected_page
-    selected_page = st.sidebar.selectbox("Explore or Predict", ("Explore", "Predict"))
+    selected_page = st.sidebar.selectbox("Explore or Predict", ("Explore", "Predict"), key="sidebar_select")
     if st.sidebar.button("Sign Out"):
         activity_log.log_activity(st.session_state.username, "User logged out")
         st.session_state.logged_in = False
         selected_page = None
-    show_sidebar_image()
+    st.sidebar.markdown("<br>" * 12, unsafe_allow_html=True)
+    st.sidebar.image(logo, use_column_width=False, width=new_size[0])
 
 def show_pages():
     if selected_page == "Explore":
@@ -42,28 +43,25 @@ def show_image():
     with col5:
         st.image(logo, width=new_size[0], use_column_width=False)
 
-def show_sidebar_image():
-    st.sidebar.markdown("<br>" * 12, unsafe_allow_html=True)
-    st.sidebar.image(logo, use_column_width=False, width=new_size[0])
-
 # Placeholder for content
 content_placeholder = st.empty()
 
 if not st.session_state.logged_in:
-    st.title('🍇 Vineyard Sign in')
+    st.title('Vineyard Sign in🔐')
     username = st.text_input("Username")
     password = st.text_input("Password", type="password")
 
     if st.button("Log In"):
-        if username in valid_username and password == valid_password:
+        if username in valid_username and password in valid_password:
             st.session_state.logged_in = True
             st.session_state.username = username
             activity_log.log_activity(username, "User logged in")
-            content_placeholder.empty()
         else:
-            st.error("Invalid credentials")
+            st.error("Invalid Credentials")
+
     show_image()
 
 if st.session_state.logged_in:
     show_sidebar()
     show_pages()
+
